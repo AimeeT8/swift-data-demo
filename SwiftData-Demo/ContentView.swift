c9ee8367b5b9fdebfea15d0f055bc324c2be7f26
aimee_t <aimeetemple14@gmail.com>
2024-09-03 19:13:00 -0400

    Added modelContext to Environment in contentView

--- SwiftData-Demo/ContentView.swift
+++ SwiftData-Demo/ContentView.swift
@@ -6,8 +6,14 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    
+    @Environment(\.modelContext) private var context
+    
+    
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
