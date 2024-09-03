1e3dc0961e3be146e19cd6fd33733258d34be50d
aimee_t <aimeetemple14@gmail.com>
2024-09-03 18:57:01 -0400

    Initial commit

---
+++ SwiftData-Demo/SwiftData_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftData_DemoApp.swift
+//  SwiftData-Demo
+//
+//  Created by Aimee Temple on 2024-09-03.
+//
+
+import SwiftUI
+
+@main
+struct SwiftData_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
